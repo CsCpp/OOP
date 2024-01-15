f573d64c5dceee034ec63bca3d4d3d8f00926de9
CppKotlin <cscppkotlin@gmail.com>
2024-01-15 22:57:52 +0300

    Добавьте файлы проекта.

---
+++ OOP/MyClass.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+
+namespace myNamespace {			// собственное пространство имен
+
+	class MyClass
+	{
+	public:
+
+		void PrintMSG(const char str[]);
+	};
+
+}
---
+++ OOP/OOP.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "Sum.h"
+#include"MyClass.h"
+
+using namespace std;
+using namespace myNamespace;
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    
+    cout << Sum(2, 6) << endl;
+
+    MyClass a;
+   
+    a.PrintMSG("msg");
+
+    return 0;
+}
