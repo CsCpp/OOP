a1653e517a1cd3ba83afaf123d61ebd3385164d0
CppKotlin <cscppkotlin@gmail.com>
2024-01-17 23:15:11 +0300

    //лямбда-выражение | лямбда-функции | анонимные функции

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -5,42 +5,14 @@
 using namespace std;
 
 
-//std::function
-void Foo(int a)
-{
-  
-    if (a > 10 && a < 40)
-    {
-        cout << "Foo - " << a << endl;
-    }
-}
+//лямбда-выражение | лямбда-функции | анонимные функции
 
-void Bar(int a)
-{
-  
-    if (a % 2 == 0) 
-    {
-        cout << "Bar - " << a << endl;
-    }
 
-}
-
-void doWork(vector<int> &mV, function<void(int)> f) 
-{
-    for (auto&& e : mV) 
-    {
-        f(e);
-    }
-}
-
-void doWork2(vector<int>& mV, vector<function<void(int)>> funcVector)
+void doWork2(vector<int>& mV, function<void(int)> func)
 {
     for (auto&& e : mV)
     {
-        for (auto& fe : funcVector) 
-        {
-            fe(e);
-        }
+        func(e);
     }
 }
 
@@ -48,20 +20,30 @@ void doWork2(vector<int>& mV, vector<function<void(int)>> funcVector)
 int main()
 {
     setlocale(LC_ALL, "ru");
-    vector<int> mV = {2,52,41,41,36,89,21,15,24,93,27,51,84};
-    cout << endl << "doWork" << endl;
-    cout << "Foo..." << endl;
-    doWork(mV, Foo);
-    cout << "Bar..." << endl;
-    doWork(mV, Bar);
-    cout <<  endl << "doWork2" << endl;
-   
-    vector<function<void(int)>> l = {Foo, Bar};
+    vector<int> mV = { 2,52,41,41,36,89,21,15,24,93,27,51,84 };
 
-    doWork2(mV, l);
-    l.emplace_back(Foo);
-    l.emplace_back(Foo);
-    doWork2(mV, l);
+    int p = 0;
+    cout << "Лямбда функция . p=" << p << endl;
+
+    auto ls = [&p]()
+        {
+            p = 3;
+        };
+    ls();
+    cout << "Лямбда функция . p=" << p << endl;
+
+
+
+    doWork2(mV,
+        [](int a) 
+        {
+            if (a % 2 == 0)
+            {
+                 cout << "ананимная функция  \\ четные числа  " << a << endl;
+            }
+        });
+
+
 
     return 0;
 }
