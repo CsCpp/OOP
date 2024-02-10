6f4e46c258b9c82f446f9ade98632b2420d411a6
CppKotlin <cscppkotlin@gmail.com>
2024-02-10 22:40:20 +0300

    //указатель

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,16 +1,22 @@
 ﻿#include <iostream>
 using namespace std;
 
-//inline ключевое слово. Встраиваемая функция.
+//указатель
 
-inline int Sum(int a, int b)
-{
-    return a + b;
-}
+
 
 int main()
 {
-    cout << Sum(3, 6) << endl;
-   
+    setlocale(LC_ALL, "ru");
+
+    int a = 9;
+    cout << &a <<"\tадрес переменной" << endl;
+    int* pa = &a;
+    cout << pa <<"\tадрес на который ссылается указатель" << endl;
+    cout << &pa <<"\tадрес самого указателя"<< endl;
+    cout << *pa <<"\t\t\tзначение которое храниться по  указателю"<< endl;
+
+
+
     return 0;
 }
