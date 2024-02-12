719a8cbcbea612673ca2d92331468c53cd375ae3
CppKotlin <cscppkotlin@gmail.com>
2024-02-12 21:28:05 +0300

    //передача аргумента по указателю

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,29 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-//ссылки
+//передача аргумента по указателю
+// как вернуть 2 и более значения из функции
 
 
+void Foo(int *pa)
+{
+    *pa+=5;
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru");
     int a = 5;
-    int* pa = &a;
-    int& aRef = a;
-
-    cout << aRef <<"  значение по ссылке" << endl;
-    cout << pa <<"  адрес переменной а" << endl;
-    cout << &aRef << "  адрес куда указывает ссылка" << endl;
-    aRef++;
-    cout << aRef << endl;
-    aRef = 55;
-    cout << aRef << endl;
     cout << a << endl;
-
-    int& aRef2 = *pa;
-    int* ppa = &aRef2;
-    *ppa = 82;
+    Foo(&a);
     cout << a << endl;
 
     return 0;
