3dc8e8c78d0d0e2214262899e6eb307726541f11
CppKotlin <cscppkotlin@gmail.com>
2024-02-13 22:02:40 +0300

    //передача аргумента по ссылке

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,22 +1,35 @@
 ﻿#include <iostream>
 using namespace std;
 
-//передача аргумента по указателю
-// как вернуть 2 и более значения из функции
+//передача аргумента по ссылке
 
 
-void Foo(int *pa)
+
+void Foo1(int a)
 {
-    *pa+=5;
+    a = 1;;
+}
+
+void Foo2(int& a)
+{
+   a=2;
+}
+
+void Foo3(int* pa)
+{
+    *pa=3;
 }
 
 int main()
 {
+    int a = -1;
     setlocale(LC_ALL, "ru");
-    int a = 5;
-    cout << a << endl;
-    Foo(&a);
-    cout << a << endl;
+    Foo1(a);
+    cout << a << endl << endl;
+    Foo2(a);
+    cout << a << endl << endl;
+    Foo3(&a);
+    cout << a << endl << endl;
 
     return 0;
 }
