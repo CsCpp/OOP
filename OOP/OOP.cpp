007b71fac7b8cd87c30472027096998edefe2b67
CppKotlin <cscppkotlin@gmail.com>
2024-02-16 23:02:53 +0300

    //двумерный динамический массив

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,22 +1,71 @@
 ﻿#include <iostream>
 using namespace std;
 
-//динамический массив
+//двумерный динамический массив
 
+void printArr(int** arr, int rows, int cols)
+{
+    cout << "-------------------------------------------------------------------------" << endl;
+    cout << "--------------------------------PRINT_arr--------------------------------" << endl;
+    cout << "-------------------------------------------------------------------------" << endl;
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "--------------------------------END print--------------------------------" << endl;
+    cout << "-------------------------------------------------------------------------" << endl;
+}
 
 
 int main()
 {
-    int size = 0;
-    cout << "Size enter" << endl;
-    cin >> size;
-    int* arr  = new int[size];
-    for (int i=0; i<size; ++i) 
+    int rows = 0;
+    int cols = 0;
+    cout << "enter ROWS: ";
+    cin >> rows;
+    cout << endl << "enter COLS: ";
+    cin >> cols;
+    cout<<endl<< endl;
+    int** arr = new int* [rows];
+
+   /* for (int i = 0; i < rows; ++i)
     {
-        arr[i] = rand()%1000;
-        cout << i<<"\t"<< * (arr + i) << endl;
+        arr[i] = new int[cols];
     }
+   */
+    //--------------------------------- Rand
+    for (int i = 0; i < rows; ++i)
+    {   
+        arr[i] = new int[cols];
+
+        for (int j = 0; j < cols; ++j)
+        {
+            arr[i][j] = rand() % 99;
+        }
+    }
+    //--------------------------------- Print
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    printArr(arr, rows, cols);
+
+
+
+    //---------------------------------
+    for (int i = 0; i < rows; ++i)
+    {
+        delete [] arr[i];
+    }
+    
     delete[] arr;
-    arr = nullptr;
     return 0;
 }
