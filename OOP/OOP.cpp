21eec2250eb48ff76532a6bd21f84fdec3ca6ffe
CppKotlin <cscppkotlin@gmail.com>
2024-02-17 23:27:22 +0300

    //константные аргументы функций

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,71 +1,32 @@
 ﻿#include <iostream>
 using namespace std;
 
-//двумерный динамический массив
+//константные аргументы функций
 
-void printArr(int** arr, int rows, int cols)
+void FillArray( int* const arr, const int size)
 {
-    cout << "-------------------------------------------------------------------------" << endl;
-    cout << "--------------------------------PRINT_arr--------------------------------" << endl;
-    cout << "-------------------------------------------------------------------------" << endl;
-    for (int i = 0; i < rows; ++i)
+    for (int i = 0; i < size; ++i)
     {
-        for (int j = 0; j < cols; ++j)
-        {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
+        arr[i] = rand() % 33;
     }
-    cout << "--------------------------------END print--------------------------------" << endl;
-    cout << "-------------------------------------------------------------------------" << endl;
 }
 
+void ShowArray(const int* const arr, const int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+}
 
 int main()
 {
-    int rows = 0;
-    int cols = 0;
-    cout << "enter ROWS: ";
-    cin >> rows;
-    cout << endl << "enter COLS: ";
-    cin >> cols;
-    cout<<endl<< endl;
-    int** arr = new int* [rows];
-
-   /* for (int i = 0; i < rows; ++i)
-    {
-        arr[i] = new int[cols];
-    }
-   */
-    //--------------------------------- Rand
-    for (int i = 0; i < rows; ++i)
-    {   
-        arr[i] = new int[cols];
-
-        for (int j = 0; j < cols; ++j)
-        {
-            arr[i][j] = rand() % 99;
-        }
-    }
-    //--------------------------------- Print
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    printArr(arr, rows, cols);
+    int size = 7;
+    int* arr = new int[size];
+    FillArray(arr, size);
+    ShowArray(arr, size);
 
 
-
-    //---------------------------------
-    for (int i = 0; i < rows; ++i)
-    {
-        delete [] arr[i];
-    }
-    
-    delete[] arr;
     return 0;
 }
