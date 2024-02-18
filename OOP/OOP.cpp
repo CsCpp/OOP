289ef52c5a502fa586f707ec4f384ca4fb790077
CppKotlin <cscppkotlin@gmail.com>
2024-02-18 21:34:34 +0300

    //копирование динамического массива

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
-//константные аргументы функций
+//копирование динамического массива
 
 void FillArray( int* const arr, const int size)
 {
@@ -24,9 +24,29 @@ int main()
 {
     int size = 7;
     int* arr = new int[size];
+    int* newArr = new int[size];
     FillArray(arr, size);
+    FillArray(newArr, size);
+
+    delete [] arr;
+    arr = nullptr;
+ 
+    arr = new int[size];
+    for (int i = 0; i < size; ++i) // arr=newArr
+    {
+        arr[i] = newArr[i];
+    }
+
+
     ShowArray(arr, size);
+    ShowArray(newArr, size);
 
 
+
+
+
+
+    delete [] arr;
+    delete [] newArr;
     return 0;
 }
