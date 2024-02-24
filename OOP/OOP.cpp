d7e836f3dd21ae3ce3bb41a30b5e8e5b02409984
CppKotlin <cscppkotlin@gmail.com>
2024-02-24 23:26:43 +0300

    //  конкатенация строк С и С++

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,33 +1,24 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
-//  строки и указатели в С++
-void Foo(const char* str)
-{
-	cout << "Foo print STR = " << str << endl;
-	cout << "размер строки, strlen(str) = " << strlen(str) << endl;
+//  конкатенация строк С и С++
 
-}
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 //-----------------------------------------------------------------------------------
-	char strOld [] = "Hello!";
-	const char* str = strOld;
-
-	cout << str << endl;
-	cout << strOld << endl;
-
-	 
-	const char* strArr[] = {"Hi","tyt","TEST"};
-	for (auto&& a : strArr)
-	{
-		cout << a << endl;
-	}
-
-	Foo(str);
-
+	char str1[255] = "Hello";
+	const char str2[255] = "World";
+	cout << str1 << endl;
+	strcat_s(str1, str2);
+	cout << str1 << endl;
+	string str3 = "Hello";
+	string str4 = "World";
+	string result;
+	result = str3 + ' ' + str4 + "! " + str1;
+	cout << result << endl;
 //-----------------------------------------------------------------------------------
 return 777;
 }
