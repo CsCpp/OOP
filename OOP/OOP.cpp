d94d0de470d617add614d19107b60c298a53664b
CppKotlin <cscppkotlin@gmail.com>
2024-02-25 16:24:31 +0300

    //  #define // препроцессор

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -2,23 +2,18 @@
 #include <string>
 using namespace std;
 
-//  конкатенация строк С и С++
+#define PI 3.14
+
+//  #define
+// препроцессор
 
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 //-----------------------------------------------------------------------------------
-	char str1[255] = "Hello";
-	const char str2[255] = "World";
-	cout << str1 << endl;
-	strcat_s(str1, str2);
-	cout << str1 << endl;
-	string str3 = "Hello";
-	string str4 = "World";
-	string result;
-	result = str3 + ' ' + str4 + "! " + str1;
-	cout << result << endl;
+	cout << PI << endl;
+
 //-----------------------------------------------------------------------------------
 return 777;
 }
