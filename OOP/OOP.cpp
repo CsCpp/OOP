9c5932ead6accc7fc1c55aafcd5ce8a9baa08474
CppKotlin <cscppkotlin@gmail.com>
2024-02-27 22:37:03 +0300

    //  int arge, char*argv[]

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -3,32 +3,20 @@
 using namespace std;
 
 
-//  тернарный оператор
+//  int arge, char*argv[]
 // 
 
 
-int main()
+void main(int argc,  char*  argv[])
 {
 	setlocale(LC_ALL, "ru");
 //-----------------------------------------------------------------------------------
-
-	int a;
-	
-	while (1)
+	for (int i = 0; i < argc; ++i)
 	{
-		cin >> a;
-		a < 10 ? cout << "a<10" << endl : a>10? cout << "a>10" << endl: cout << "a=10" << endl;
-/*
-		if (a < 10)
-		{
-			cout << "a<10" << endl;
-		}
-		else
-		{
-			cout << "a>10" << endl;
-		}
-*/
+		cout << argv[i] << endl;
+
 	}
+	system ("pause");
 //-----------------------------------------------------------------------------------
-return 777;
+
 }
